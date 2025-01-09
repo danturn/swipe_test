ff60bbb19b291a2e964d6c66fa52e6e671be2020
dan <dan@getsona.com>
2025-01-09 21:35:59 +0000

    use reanimated view

--- app/index.tsx
+++ app/index.tsx
@@ -2,17 +2,18 @@ import { useFocusEffect } from "expo-router";
 import { useState, useCallback, useEffect } from "react";
 import { Text } from "react-native";
 import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
-import { SharedValue } from "react-native-reanimated";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-export const getWorkouts = [
-  {
-    id: 0,
-    startDatetime: "2024-01-01",
-    endDatetime: "2024-01-01",
-  },
-];
+import Reanimated, {
+  SharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
 export default function Index() {
-  useFocusEffect(useCallback(() => {}, []));
+  useFocusEffect(
+    useCallback(() => {
+      console.log("test");
+    }, [])
+  );
 
   return (
     <GestureHandlerRootView>
@@ -24,5 +25,9 @@ export default function Index() {
 }
 
 function RightAction(prog: SharedValue<number>, drag: SharedValue<number>) {
-  return <Text>Delete</Text>;
+  return (
+    <Reanimated.View>
+      <Text>Text</Text>
+    </Reanimated.View>
+  );
 }
